9dbd4266c5a9e5f20eac3d60fa84eaa07f7e5533
Caio Monsores <caio.monsoress@gmail.com>
2023-10-18 20:43:28 -0300

    atualizacao da aula

--- brincando_com_repositorio.py
+++ brincando_com_repositorio.py
@@ -1,13 +1,24 @@
 import repositorio
 
-#EXPERIMENTE A FUNÇÃO retornar_produtos(), que retornar um dicionário de dicionários com todos os produtos
+# #EXPERIMENTE A FUNÇÃO retornar_produtos(), que retornar um dicionário de dicionários com todos os produtos
+# print (repositorio.retornar_produtos())
+# #EXPERIMENTE A FUNÇÃO retornar_produto(id), que recebe um id e retorna um único dicionário contendo os detalhes do produto
+# print(repositorio.retornar_produto(4))
+# #EXPERIMENTE A FUNÇÃO remover_produto(id), que recebe um id e remove um produto do dicionário de dicionários
+# repositorio.remover_produto(1)
+# print(repositorio.retornar_produtos())
+# #EXPERIMENTE A FUNÇÃO alterar_produto(id, dados_produto), que recebe um id e um dicionário contendo dados do produto para então atualizar o produto no dicionário de dicionários
+# alterado = {
+#   "nome" : "ALTERADO",
+#   "descricao" : "DESCRICAO ALTERDA",
+#   "preco" : "00",
+#   "imagem" : "arquivo.jpg"
+# }
 
-#EXPERIMENTE A FUNÇÃO retornar_produto(id), que recebe um id e retorna um único dicionário contendo os detalhes do produto
+# repositorio.atualizar_produto(2, alterado)
+# print (repositorio.retornar_produto())
+# #EXPERIMENTE A FUNÇÃO criar_produto(nome, descricao, preco, imagem), que recebe um nome, uma descricao, um preço e uma imagem e adiciona o produto no dicionário de dicionário com um novo id
 
-#EXPERIMENTE A FUNÇÃO remover_produto(id), que recebe um id e remove um produto do dicionário de dicionários
-
-#EXPERIMENTE A FUNÇÃO alterar_produto(id, dados_produto), que recebe um id e um dicionário contendo dados do produto para então atualizar o produto no dicionário de dicionários
-
-#EXPERIMENTE A FUNÇÃO criar_produto(nome, descricao, preco, imagem), que recebe um nome, uma descricao, um preço e uma imagem e adiciona o produto no dicionário de dicionário com um novo id
-
-#EXPERIMENTE A FUNÇÃO gerar_id(), que retornar um novo id à partir dos ids já cadastrados no dicionário de dicionários
+# repositorio.criar_produto("teste", "teste", 150, "imagem")
+# print(repositorio.retornar_produtos)
+# #EXPERIMENTE A FUNÇÃO gerar_id(), que retornar um novo id à partir dos ids já cadastrados no dicionário de dicionários
